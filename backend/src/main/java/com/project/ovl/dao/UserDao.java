8bc481eb236e1f0b7211b6d2bf250ece9354df14
GyuYoungCho <rbud613@gmail.com>
2021-07-23 21:42:13 +0900

    S05P12A606-38 add : user management

--- backend/src/main/java/com/project/ovl/dao/UserDao.java
+++ backend/src/main/java/com/project/ovl/dao/UserDao.java
@@ -1,6 +1,8 @@
 
 package com.project.ovl.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.ovl.model.user.User;
@@ -8,5 +10,6 @@ import com.project.ovl.model.user.User;
 public interface UserDao extends JpaRepository<User, String> {
 	 User getUserByEmail(String email);
 	 User getUserByNickname(String nickname);
-	 
+	 User getUserByUserid(int userid);
+	 Optional<User> findUserByNameAndPhone(String name, String phone);
 }
--- backend/src/main/java/com/project/ovl/model/user/User.java
+++ backend/src/main/java/com/project/ovl/model/user/User.java
@@ -3,6 +3,7 @@ package com.project.ovl.model.user;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -26,7 +27,8 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int user_id;
+    @Column(name = "user_id")
+    private int userid;
 
     @JsonIgnore
     private String email;
