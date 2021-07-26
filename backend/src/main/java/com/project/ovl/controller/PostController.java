d10a7dde45a96fefbbcac6e962a2663ec417638d
류희영 <dbgmldud8313@naver.com>
2021-07-26 13:58:02 +0900

    S05P12A606-79 add : PostController

--- backend/src/main/java/com/project/ovl/controller/PostController.java
+++ backend/src/main/java/com/project/ovl/controller/PostController.java
@@ -1,5 +1,12 @@
 package com.project.ovl.controller;
 
-public class PostController {
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/post")
+@CrossOrigin("*")
+public class PostController {
+	private static final String SUCCESS = "success";
 }
