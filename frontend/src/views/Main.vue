7f4257fec65552c45f275eda9a2479b65fb18e9e
kevin622 <kevin622@yonsei.ac.kr>
2021-07-26 10:28:44 +0900

    S05P12A606-58 add: welcome page

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <h1>NEWSFEED</h1>
+    <!-- <h1>MAIN PAGE</h1> -->
+    <Newsfeed v-if="isLoggedIn" />
+    <Welcome v-else />
   </div>
 </template>
 
 <script>
-export default {
+import Newsfeed from '@/components/Newsfeed.vue'
+import Welcome from '@/components/Welcome.vue'
 
+export default {
+  components: {
+    Newsfeed, Welcome,
+  },
+  data: () => ({
+    isLoggedIn: false,
+  })
 }
 </script>
 
