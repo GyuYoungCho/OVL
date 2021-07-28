4c5a3314bcc5c9c61be3aef54ad0f7a49bac08c2
GyuYoungCho <rbud613@gmail.com>
2021-07-28 13:06:44 +0900

    fix : solve conflict

---
+++ backend/src/main/java/com/project/ovl/model/user/User.java
@@ -0,0 +1,42 @@
+package com.project.ovl.model.user;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userid;
+
+    
+    private String email;
+    private String nickname;
+    private String name;
+    private String phone;
+    private String password;
+    private int experience;
+    private int account_open;
+    private int warning;
+    
+    private String original_file_name;
+    private String stored_file_path;
+}
