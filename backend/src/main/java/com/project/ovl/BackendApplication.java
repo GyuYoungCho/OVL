475dee4240dd42f1112955fdbf23e83839e7e93a
GyuYoungCho <rbud613@gmail.com>
2021-08-02 09:18:45 +0900

    S05P12A606-147 add : challenge schedular

--- backend/src/main/java/com/project/ovl/BackendApplication.java
+++ backend/src/main/java/com/project/ovl/BackendApplication.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,6 +14,7 @@ import com.project.ovl.interceptor.JwtInterceptor;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendApplication implements WebMvcConfigurer{
 
 	
--- backend/src/main/java/com/project/ovl/util/ChallengeSchedular.java
+++ backend/src/main/java/com/project/ovl/util/ChallengeSchedular.java
@@ -1,18 +1,94 @@
 package com.project.ovl.util;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.project.ovl.dao.ChallengeCertificationDao;
+import com.project.ovl.dao.ChallengeDao;
+import com.project.ovl.dao.UserDao;
+import com.project.ovl.model.challenge.Challenge;
+import com.project.ovl.model.challenge.ChallengeCertification;
+import com.project.ovl.model.user.User;
+
 @Component
 public class ChallengeSchedular {
-	@Scheduled(cron = "0 5 * * * *")
-	   public void cronJobSch() {
-	      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-	      Date now = new Date();
-	      String strDate = sdf.format(now);
-	      System.out.println("Java cron job expression:: " + strDate);
-	   }
+
+	@Autowired
+	ChallengeDao challengeDao;
+
+	@Autowired
+	ChallengeCertificationDao challengeCertificationDao;
+
+	@Autowired
+	UserDao userDao;
+
+	@Scheduled(cron = "0 17 * * * *")
+	public void cronJobSch() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date now = new Date();
+		String strDate = sdf.format(now);
+		System.out.println(strDate);
+		List<Challenge> challList = challengeDao.findAll();
+
+		for (Challenge ch : challList) {
+			// 챌린지 시작할 게 있을 경우
+			if (sdf.format(ch.getStart_date()).equals(strDate)) {
+				Optional<List<User>> attendList = userDao.findByChallengeIdChallengeId(ch.getChallengeId());
+
+				if (attendList.isPresent()) {
+
+					int dat = ch.getPeriod() / ch.getCycle();
+
+					for (User user : attendList.get()) {
+						
+						System.out.println(user.getUserid());
+						
+						Calendar cal = Calendar.getInstance();
+						cal.setTime(ch.getStart_date());
+
+						for (int i = 0; i < dat; i++) {
+							challengeCertificationDao.save(new ChallengeCertification(0, user, ch, cal.getTime(), 0));
+							cal.add(Calendar.DATE, ch.getCycle());
+						}
+
+					}
+				}
+			}
+
+			// 챌린지 끝날 게 있을 경우
+
+			Calendar endcal = Calendar.getInstance();
+			endcal.setTime(ch.getStart_date());
+			endcal.add(Calendar.DATE, ch.getPeriod());
+			if (sdf.format(endcal.getTime()).equals(strDate)) {
+				Optional<List<User>> attendList = userDao.findByChallengeIdChallengeId(ch.getChallengeId());
+
+				if (attendList.isPresent()) {
+					e:for (User user : attendList.get()) {
+						List<ChallengeCertification> ccList = challengeCertificationDao.findByUserId(user);
+						
+						for(ChallengeCertification c : ccList) {
+							if(c.getCertification()==0) continue e;
+						}
+						//모든 챌린지 완료시 여기로
+						
+						//여기서 complete 처리..?
+						
+						
+						// 인증 기록 db 지우기
+						for(ChallengeCertification c : ccList) {
+							challengeCertificationDao.delete(c);
+						}
+					}
+				}
+			}
+		}
+	}
 }
