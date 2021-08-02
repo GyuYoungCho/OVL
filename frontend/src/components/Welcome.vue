4c9952877b248dec9e51038e0aeeddef133b90de
kevin622 <kevin622@yonsei.ac.kr>
2021-08-02 22:44:22 +0900

    S05P13A606-26 add: carousel 실험

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="bg-vegan">
     <v-container>
+      <Carousel />
       <h1 class="text-center">Welcome Page</h1>
       <h2 class="text-center vertical-center">슬로건과 기업 소개 써 넣을 자리</h2>
-      <img src="C:\Users\multicampus\git\S05P12A606\backend\src\main\resources\static\post\15\13726747712500.png" alt="">
+      <!-- <img src="C:\Users\multicampus\git\S05P12A606\backend\src\main\resources\static\post\15\13726747712500.png" alt=""> -->
       <div class="text-center">
         <v-btn
           class="mx-3"
@@ -23,7 +24,11 @@
 </template>
 
 <script>
+import Carousel from './Carousel.vue'
 export default {
+  components: {
+    Carousel,
+  },
   data: () => ({
   }),
   methods: {
@@ -39,11 +44,18 @@ export default {
 
 <style scoped>
   .bg-vegan {
-    background-image: url('../assets/image/background1.jpg');
-    height: 700px;
+    background: url('../assets/image/background1.jpg') center; 
+    background-size: cover;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    overflow: auto;
   }
   .vertical-center {
     margin-top: 100px;
     margin-bottom: 100px;
   }
+
 </style>
