d5fda35b7a461d97ad230df15ebfaa761355ceec
seohui548 <seohui548@gmail.com>
2021-08-03 16:39:56 +0900

    add: user login

--- frontend/src/store/modules/user.js
+++ frontend/src/store/modules/user.js
@@ -1,17 +1,87 @@
 // @/store/modules/user.js
+import axios from "axios";
+import API from '@/api/index.js'
+import userAPI from '@/api/user.js'
 
 export default {
-  namespaced: true,
+namespaced: true,
 	state: {
+		userinfo: {
+			email: "",
+			password: "",
+			name: "",
+			nickname: "",
+			phone: "",
+		},
+		isLogin: false,
 
 	},
 	mutations: {
+		setUserInfo(state, payload) {
+			state.isLogin = true;
+			state.userinfo = payload;
+		},
+		setLogout(state) {
+			state.isLogin = true;
+			state.userinfo = {
+				email: "",
+				password: "",
+				name:"",
+				nickname: "",
+				phone: "",
+			};
+			localStorage.removeItem("access-token");
+		},
 
 	},
 	getters: {
-    
-  },
-  actions: {
-    
-  },
+		userinfo(state) {
+			return state.userinfo;
+		},
+		isLogin(state) {
+			return state.isLogin;
+		},
+	},
+	actions: {
+	getUserInfo(store) {
+			let token = localStorage.getItem("access-token");
+			if (!token) {
+				return;
+			}
+			axios({
+				method: "post",
+				url: "http://localhost:3000/user/modify_user",
+				headers: { "access-token" : token}
+			}).then((res) => {
+				console.log(res.data.User.nickname);
+				if (res.data) {
+					store.commit("setUserInfo", res.data.User);
+				}
+				else
+					console.log("실패.");
+			}).catch((err) => {
+				console.log(err);
+			})
+	},
+		login(store, loginObj) {
+			console.log("확인한다 로그인");
+			return new Promise(function (resolve) {
+			axios({
+				method: "post",
+				url: API.url + userAPI.login(),
+				data: loginObj,
+			}).then((res) => {
+				localStorage.setItem("access-token", res.header["access-token"]);
+				resolve();
+			}).catch((err) => {
+				alert("이메일과 비밀번호를 확인하세요.");
+				console.log(err);
+			});
+		})
+	},
+		logout(store) {
+			store.commit('setLogout');
+	},
+
+}
 }
