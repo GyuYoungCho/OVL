caabda0f4aa4cd404a4c3bbe56e695bfbcfaf106
kevin622 <kevin622@yonsei.ac.kr>
2021-08-07 23:17:36 +0900

    S05P13A606-80 add: recipe create css

--- frontend/src/views/RecipeCreate.vue
+++ frontend/src/views/RecipeCreate.vue
@@ -1,71 +1,65 @@
 <template>
-  <div>
-    <h1>레시피 생성 페이지</h1>
-    <input type="text" v-model="title" placeholder="요리 이름">
-    <div>
-      <label for="pictureImageInput" v-if="!picture">대표 사진 추가</label>
-      <input type="file" ref="file" multiple @change="pictureInputChange" id="pictureImageInput">
-      <img :src="picture.previewURL" v-if="!!picture" alt="" class="preview-img">
-      
-    </div>
-    <textarea cols="30" rows="10" placeholder="요리 간단 설명" v-model="content"></textarea>
-    <v-tabs
-      v-model="tab"
-      align-with-title
-      color="green darken-4"
-      centered
-    >
-      <v-tabs-slider color="green darken-4"></v-tabs-slider>
-      <v-tab>재료</v-tab>
-      <v-tab>과정</v-tab>
+  <v-container>
+    <section class="recipeCreate">
+      <h1>레시피 작성</h1>
+      <!-- 요리 이름 -->
+      <div>
+        <input type="text" v-model="title" placeholder="요리 이름">
+      </div>
+      <!-- 대표 사진 -->
+      <div class="recipeCreatePic" v-if="!picture">
+        <label for="pictureImageInput"><v-icon>mdi-plus</v-icon></label>
+        <input type="file" ref="file" multiple @change="pictureInputChange" id="pictureImageInput">
+      </div>
+      <div v-else class="img-container">
+        <img :src="picture.previewURL" alt="">
+      </div>
 
-    </v-tabs>
-    <v-tabs-items v-model="tab">
-      <!-- 재료 탭 내용 -->
-      <v-tab-item>
-        <!-- 재료는 글로만 처리할 수도, 사진을 첨부할 수도 있음 -->
-        <!-- 일단 사진 첨부 안 한다고 가정! -->
-        <textarea name="" id="" cols="30" rows="10" v-model="ingredient" placeholder="재료 쓰는 곳"></textarea>
+      <!-- 요리 설명 -->
+      <textarea cols="30" rows="10" placeholder="요리 간단 설명" v-model="content"></textarea>
 
-        <!-- 재료에 사진을 첨부할 경우 -->
-        <!-- <div class="py-6">
-          <label for="previewIngredientImageInput">재료 이미지 추가</label>
-          <input type="file" ref="files" multiple @change="onIngredientInputChange" id="previewIngredientImageInput">
-        </div> -->
-
-        <!-- 사진 미리보기 -->
-        <!-- <div v-for="(ingredientImgFile, index) in ingredientImgFiles" :key="index" >
-          <img class="preview-img" :src="ingredientImgFile.previewURL" alt="" @click="deleteIngredientImg(ingredientImgFile.number)">
-        </div> -->
-
-      </v-tab-item>
-      
-      <!-- 과정 탭 내용 -->
-      <v-tab-item>
-        <!-- 과정에 사진을 첨부(필수) -->
-        <div class="py-6">
-          <label for="previewProcessImageInput">과정 이미지 추가</label>
-          <input type="file" ref="files" multiple @change="onProcessInputChange" id="previewProcessImageInput">
+      <!-- 재료와 과정 구분 탭 -->
+      <article>
+        <button :class="{ 'tabBtn': isIngredient, 'unselectedTabBtn': !isIngredient }" @click="onTabBtnClick">재료</button>
+        <button :class="{ 'tabBtn': !isIngredient, 'unselectedTabBtn': isIngredient }" @click="onTabBtnClick">과정</button>
+      </article>
+      <div class="tab-div">
+        <!-- 재료 탭 -->
+        <div v-if="isIngredient">
+          <textarea name="" id="" cols="30" rows="10" placeholder="재료를 적어주세요." class="ingredient-text" v-model="ingredient">
+          </textarea>
         </div>
-
-        <!-- 과정 사진 미리보기 -->
-        <div v-for="(processImgFile, index) in processImgFiles" :key="index" >
-          <img class="preview-img" :src="processImgFile.previewURL" alt="" @click="deleteProcessImg(processImgFile.number)">
-          <textarea cols="30" rows="10" v-model="processImgFile.text"></textarea>
+        <!-- 과정 탭 -->
+        <div class="process-tab" v-else>
+          <div v-for="(processImgFile, index) in processImgFiles" :key="index" >
+            <div class="process-index">
+              <span># {{ index + 1 }}</span>
+              <button class="cancelBtn" @click="deleteProcessImg(processImgFile.number)">X</button>
+            </div>
+            <div class="process-data">
+              <div class="preivew-img-container">
+                <img class="preview-img" :src="processImgFile.previewURL" alt="" >
+              </div>
+              <textarea cols="30" rows="10" v-model="processImgFile.text"></textarea>
+            </div>
+          </div>
+          <div>
+            <p># next</p>
+            <div class="process-input">
+              <label for="processImgInput"><v-icon>mdi-plus</v-icon></label>
+              <input type="file" id="processImgInput" ref="files" multiple @change="onProcessInputChange">
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+          </div>
         </div>
-
-      </v-tab-item>
-    </v-tabs-items>
-
-    <div>
-      <v-btn
-        @click="onRecipeBtnClick"
-        :disabled="!valid"
-      >
-        <button >레시피 작성 완료</button>
-      </v-btn>
-    </div>
-  </div>
+      </div>
+      <div>
+        <button @click="onRecipeBtnClick" class="finalBtn" :class="{ 'bg-freditgreen': valid, 'disabledBtn': !valid }">
+          레시피 작성 완료
+        </button>
+      </div>
+    </section>
+  </v-container>
 </template>
 
 
@@ -77,6 +71,7 @@ import recipeAPI from '@/api/recipe.js'
 
 export default {
   data: () => ({
+    isIngredient: true,
     title: "",
     picture: null,
     content: "",
@@ -87,6 +82,10 @@ export default {
     tab: null,
   }),
   methods: {
+    onTabBtnClick () {
+      this.isIngredient = !this.isIngredient
+    },
+
     pictureInputChange () {
       const picture = this.$refs.file.files[0]
       picture.previewURL = URL.createObjectURL(picture)
@@ -184,14 +183,7 @@ export default {
 
 
 <style scoped>
-  input[type='file'] {
-      position: absolute;
-      width: 0;
-      height: 0;
-      padding: 0;
-      overflow: hidden;
-      border: 0;
-  }
+  
   label {
     cursor: pointer;
     /* padding: 10px 20px;
@@ -202,8 +194,5 @@ export default {
     cursor: pointer;
     border-radius: 5px; */
   }
-  .preview-img {
-    height: 100px;
-    display: inline-block;
-  }
+  
 </style>
