9e294cdd1820461ca0ca88ae6f31a7296fe049ca
GyuYoungCho <rbud613@gmail.com>
2021-08-08 12:04:43 +0900

    S05P13A606-88 add : profilename component

---
+++ frontend/src/components/basic/ProfileName.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="d-flex">
+        <v-list-item-avatar size=25>
+            <v-img
+            
+            :src="profile"
+            ></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+            <v-list-item-subtitle v-text="user.nickname">
+            </v-list-item-subtitle>
+        </v-list-item-content>
+    </div>
+</template>
+
+<script>
+import API from "@/api/index.js";
+
+export default {
+    props: {
+        user : Object,
+    },
+    computed:{
+        profile() {
+            return API.url + "/profile" + this.user.userid + "/"+ this.user.stored_file_path.split('/').reverse()[0]
+        },
+    }
+}
+</script>
