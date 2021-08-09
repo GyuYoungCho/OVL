0534828f440e1be22c004d517adf1f3c3945f607
kevin622 <kevin622@yonsei.ac.kr>
2021-08-09 21:33:41 +0900

    S05P13A606-81 add: css 아직 넣는 중

--- frontend/src/store/modules/recipe.js
+++ frontend/src/store/modules/recipe.js
@@ -7,6 +7,8 @@ export default {
   // namspaced: true,
   state: {
     recipe: {},
+    recipes: [],
+    recipeLikeList: [],
     // 레시피 과정이 담기기 때문에 리스트 형식, 각 요소는 객체
     recipeDetail: [],
     recipeComments: [],
@@ -14,6 +16,12 @@ export default {
   getters: {
     recipe (state) {
       return state.recipe
+    },
+    recipes (state) {
+      return state.recipes
+    },
+    recipeLikeList (state) {
+      return state.recipeLikeList
     },
     recipeDetail (state) {
       return state.recipeDetail
@@ -23,6 +31,31 @@ export default {
     }
   },
   mutations: {
+    SET_RECIPES (state, recipes) {
+      state.recipes = recipes
+    },
+    SORT_RECIPES (state, option) {
+      const recipes = state.recipes
+      if (option==="new") {
+        recipes.sort(function (recipe1, recipe2) {
+          const time1 = new Date(recipe1.time)
+          const time2 = new Date(recipe2.time)
+          return time2 - time1
+        })
+      } else if (option==="like") {
+        recipes.sort(function (recipe1, recipe2) {
+          return recipe2.like_count - recipe1.like_count
+        })
+      } else if (option==="comment") {
+        recipes.sort(function (recipe1, recipe2) {
+          return recipe2.comment_count - recipe1.comment_count
+        })
+      }
+      state.recipes = recipes
+    },
+    SET_RECIPE_LIKE_LIST (state, recipeLikeList) {
+      state.recipeLikeList = recipeLikeList
+    },
     SET_RECIPE_DETAIL (state, recipeDetail) {
       state.recipeDetail = recipeDetail
       state.recipe = recipeDetail[0].recipeId    },
@@ -31,6 +64,48 @@ export default {
     }
   },
   actions: {
+    fetchRecipes ({ commit }) {
+      const URL = API.url + recipeAPI.select_all()
+      axios.get(URL)
+        .then(res => {
+          const data = res.data.sort(function(recipe1, recipe2) {
+            const time1 = new Date(recipe1.time)
+            const time2 = new Date(recipe2.time)
+            return time2 - time1
+          })
+          commit('SET_RECIPES', data)
+          this.recipes = data
+        })
+        .catch(err => console.error(err))
+    },
+
+    sortRecipes({ commit }, option) {
+      commit('SORT_RECIPES', option)
+    },
+
+    fetchRecipeLikeList ({ commit }, userId) {
+      const URL = API.url + recipeAPI.like_list(userId)
+      axios.get(URL)
+        .then(res => {
+          console.log(res)
+          commit('SET_RECIPE_LIKE_LIST', res.data)
+        })
+        .catch(err => console.error(err))
+    },
+
+    likeRecipe({ dispatch }, { userId, recipeId }) {
+      const URL = API.url + recipeAPI.like(userId, recipeId)
+      axios.get(URL)
+        .then(res => {
+          console.log(res)
+          dispatch('fetchRecipeLikeList', userId)
+          dispatch('fetchRecipes')
+          dispatch('fetchRecipeDetail', recipeId)
+        })
+        .catch(err => console.error(err))
+        
+    },
+
     fetchRecipeDetail ({ commit }, recipeId) {
       const URL = API.url + recipeAPI.select_detail(recipeId)
       axios.get(URL)
@@ -39,6 +114,7 @@ export default {
         })
         .catch(err => console.error(err))
     },
+
     fetchRecipeComments ({ commit }, recipeId) {
       commit
       const URL = API.url + recipeCommentAPI.search_all(recipeId)
@@ -55,7 +131,6 @@ export default {
       const URL = API.url + recipeCommentAPI.regist()
       console.log(URL)
  
-
       axios.post(URL, data)
         .then(res => {
           console.log(res)
--- frontend/src/views/RecipeDetail.vue
+++ frontend/src/views/RecipeDetail.vue
@@ -1,38 +1,99 @@
 <template>
-  <div>
-    <h1>Recipe Detail 페이지입니다.</h1>
-    <h2>대표사진</h2>
-    <img :src="srcPath(recipe)" alt="">
-    <p>title : {{ recipe.title }}</p>
-    <p>author : {{ recipe.userid.nickname }}</p>
-
-    <h2>재료</h2>
-    <p>{{ recipe.ingredient }}</p>
-
-    <h2>과정</h2>
-    <div v-for="(process, idx) in recipeDetail" :key="idx">
-      <img :src="processSrcPath(recipe, process)" alt="">
-      <p>내용 : {{ process.content }}</p>
-      
+<v-container>
+  <section class="recipeDetail">
+    <!-- 레시피 작성자 정보 -->
+    <div class="header">
+      <ProfileName :user="recipe.userid"></ProfileName>
+      <div>
+        <!-- 좋아요, 댓글수 -->
+        <v-icon class="likedHeart" v-if="recipeLikeList.includes(recipe.recipeId)" @click="onRecipeLikeBtnClick">mdi-heart</v-icon>
+        <v-icon class="unlikedHeart" v-else @click="onRecipeLikeBtnClick">mdi-heart-outline</v-icon>
+        {{ recipe.like_count }}
+        <v-icon class="chatIcon">mdi-chat-outline</v-icon>
+        {{ recipe.comment_count }}
+      </div>
     </div>
+    <div>
+      {{ recipe.title}}
+    </div>
+    <!-- 대표 사진 -->
+    <img :src="srcPath(recipe)" alt="" class="recipePic">
+
+    <!-- 탭 부분 -->
+    <div class="recipeTab">
+      <button :class="{'recipeTabBtn': tab==='ingredient', 'unselectedBtn': tab!=='ingredient'}" @click="onRecipeTagBtnClick('ingredient')">재료</button>
+      <button :class="{'recipeTabBtn': tab==='process', 'unselectedBtn': tab!=='process'}" @click="onRecipeTagBtnClick('process')">과정</button>
+      <button :class="{'recipeTabBtn': tab==='comment', 'unselectedBtn': tab!=='comment'}" @click="onRecipeTagBtnClick('comment')">댓글</button>
+    </div>
+    <div class="recipeTabContent">
+      <!-- 재료 탭 -->
+      <div class="recipeIngredient" v-if="tab==='ingredient'">
+        {{ recipe.ingredient }}
+      </div>
+
+      <!-- 과정 탭 -->
+      <div class="recipeProcess" v-if="tab==='process'">
+        <div v-for="(process, idx) in recipeDetail" :key="idx">
+          <hr v-if="idx!==0">  
+          <div class="oneProcess">
+            <img :src="processSrcPath(recipe, process)" alt="" class="processPic">
+            <p class="processContent">{{ process.content }}</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- 댓글 탭 -->
+      <div class="recipeComment" v-if="tab==='comment'">
+        <div class="commentRegisterForm">
+          <v-icon size="10vw" color="black">mdi-subdirectory-arrow-right</v-icon>
+          <div class="commentInputForm">
+            <input type="text" class="commentInput" placeholder="댓글 입력">
+            <button>ad</button>
+          </div>
+        </div>
+        {{ recipeComments }}
+        
+      </div>
+
+    </div>
+
 
     <h2>댓글</h2>
     <p>{{ recipeComments }}</p>
     <input v-model="content" type="text" placeholder="댓글달기">
     <button @click="onRegistCommentBtnClick">댓글 등록</button>
 
-  </div>
+  </section>
+
+</v-container>
 </template>
 
 <script>
+import ProfileName from '@/components/basic/ProfileName.vue'
 import { mapActions, mapGetters } from 'vuex'
 
 export default {
+  components: {
+    ProfileName,
+  },
   data: () => ({
     content: "",
+    tab: "ingredient",
   }),
   methods: {
-    ...mapActions(['registComment', ]),
+    ...mapActions(['registComment', 'likeRecipe',]),
+    onRecipeLikeBtnClick () {
+      const data = {
+        userId: this.userinfo.userid,
+        recipeId: this.recipe.recipeId,
+      }
+      this.likeRecipe(data)
+    },
+
+    onRecipeTagBtnClick (option) {
+      this.tab = option
+    },
+
     srcPath(recipe) {
       return "http://localhost:8080/recipe/" + recipe.recipeId+ "/" + recipe.stored_file_path.split('/').reverse()[0];
     },
@@ -46,7 +107,7 @@ export default {
     
   },
   computed: {
-    ...mapGetters(['recipe', 'recipeDetail', 'recipeComments',]),
+    ...mapGetters(['recipe', 'recipeDetail', 'recipeComments', 'recipeLikeList', ]),
     ...mapGetters("user", ["userinfo",]),
     comment () {
       const data = {
