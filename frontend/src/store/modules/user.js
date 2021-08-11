03d5424bcc0a3f388032a9311ca274113e42bb09
GyuYoungCho <rbud613@gmail.com>
2021-08-12 02:43:55 +0900

    fix : user.js

--- frontend/src/store/modules/user.js
+++ frontend/src/store/modules/user.js
@@ -4,32 +4,32 @@ import API from "@/api/index.js";
 import userAPI from "@/api/user.js";
 
 export default {
-namespaced: true,
-state: {
+  namespaced: true,
+  state: {
     userinfo: {
-		userid: "",
-		email: "",
-		password: "",
-		name: "",
-		nickname: "",
-		phone: "",
-		filepath: "",
-		challengeId: {
-			start_date: null,
+      userid: "",
+      email: "",
+      password: "",
+      name: "",
+      nickname: "",
+      phone: "",
+      filepath: "",
+      challengeId: {
+        start_date: null,
+      },
     },
-    },
-    rank:null,
+    rank: null,
     isLogin: false,
-},
-mutations: {
+  },
+  mutations: {
     setUserInfo(state, payload) {
-        state.isLogin = true;
-        state.userinfo = payload;
-        console.log("유저 : ", state.userinfo.nickname);
+      state.isLogin = true;
+      state.userinfo = payload;
+      console.log("유저 : ", state.userinfo.nickname);
     },
     setLogout(state) {
-    state.isLogin = false;
-    state.userinfo = {
+      state.isLogin = false;
+      state.userinfo = {
         userid: "",
         email: "",
         password: "",
@@ -37,134 +37,131 @@ mutations: {
         nickname: "",
         phone: "",
         filepath: "",
-    };
-    localStorage.removeItem("access-token");
+      };
+      localStorage.removeItem("access-token");
     },
     setReset(state) {
-        state.userinfo = {
-            userid: "",
-            email: "",
-            password: "",
-            name: "",
-            nickname: "",
-            phone: "",
-            filepath: "",
-            challengeId: {
-                start_date: null,
-            },
-            rank: null,
-        };
-    },
-        
-    setUserRank(state, payload) {
-        state.rank = payload;
+      state.userinfo = {
+        userid: "",
+        email: "",
+        password: "",
+        name: "",
+        nickname: "",
+        phone: "",
+        filepath: "",
+        challengeId: {
+          start_date: null,
+        },
+        rank: null,
+      };
     },
 
-},
-getters: {
+    setUserRank(state, payload) {
+      state.rank = payload;
+    },
+  },
+  getters: {
     userinfo(state) {
-    return state.userinfo;
+      return state.userinfo;
     },
     isLogin(state) {
-    return state.isLogin;
+      return state.isLogin;
     },
-},
-actions: {
+  },
+  actions: {
     getUserInfo(store) {
-        let token = localStorage.getItem("access-token");
-        //console.log("jwt 정보" + token);
-        if (!token) {
-            return;
-        }
-        axios({
-            method: "post",
-            url: API.url + userAPI.info(),
-            headers: { "access-token": token },
-        })
+      let token = localStorage.getItem("access-token");
+      //console.log("jwt 정보" + token);
+      if (!token) {
+        return;
+      }
+      axios({
+        method: "post",
+        url: API.url + userAPI.info(),
+        headers: { "access-token": token },
+      })
         .then((res) => {
-            if (res.data) {
+          if (res.data) {
             store.commit("setUserInfo", res.data.UserDto);
             this.state.isLogin = true;
             //console.log(this.state);
-        } else console.log("실패.");
+          } else console.log("실패.");
         })
         .catch((err) => {
-            console.log(err);
+          console.log(err);
         });
     },
     login(store, loginObj) {
-        return new Promise(function(resolve) {
-            axios({
-                method: "post",
-                url: API.url + userAPI.login(),
-                data: loginObj,
-            })
-            .then((res) => {
-                localStorage.setItem("access-token", res.headers["access-token"]);
-                resolve();
-                store.commit("setUserInfo", res.data.data);
-            })
-            .catch((err) => {
-                alert("이메일과 비밀번호를 확인하세요.");
-                console.log(err);
-            });
-        });
+      return new Promise(function(resolve) {
+        axios({
+          method: "post",
+          url: API.url + userAPI.login(),
+          data: loginObj,
+        })
+          .then((res) => {
+            localStorage.setItem("access-token", res.headers["access-token"]);
+            resolve();
+            store.commit("setUserInfo", res.data.data);
+          })
+          .catch((err) => {
+            alert("이메일과 비밀번호를 확인하세요.");
+            console.log(err);
+          });
+      });
     },
     logout(store) {
-        store.commit("setLogout");
+      store.commit("setLogout");
     },
     deleteUser(store) {
-        let token = localStorage.getItem("access-token");
-        axios({
-            method: "delete",
-            url: API.url + userAPI.delete(store.state.userinfo.userid),
-            headers: {"access-token" : token}
-        }).then(() => {
-            alert("탈퇴가 정상적으로 처리 되었습니다.");
-            store.commit("setLogout");
-        }).catch((err) => {
-            console.log(err);
+      let token = localStorage.getItem("access-token");
+      axios({
+        method: "delete",
+        url: API.url + userAPI.delete(store.state.userinfo.userid),
+        headers: { "access-token": token },
+      })
+        .then(() => {
+          alert("탈퇴가 정상적으로 처리 되었습니다.");
+          store.commit("setLogout");
         })
-    },
-    getUserRank(store, payload) {
-        let token = localStorage.getItem("access-token");
-        axios({
-            method: "get",
-            url: API.url + userAPI.rank(payload),
-            headers: {"access-token" : token}
-        }).then((res) => {
-            if (res) {
-                // console.log(res.data.rank);
-                store.commit("setUserRank", res.data.rank);
-                this.state.rank = res.rank;
-            }
-            else
-                console.log("랭크 가져오기 실패.");
-        }).catch((err) => {
-            console.log(err);
-        })
-    },
-    getUpdateUserInfo(store, payload) {
-        let token = localStorage.getItem("access-token");
-        console.log("token : ", token);
-        axios({
-            method: "get",
-            url: API.url + userAPI.tokenUpdate(payload),
-            headers: {"access-token" : token}
-        })
-        .then((res) => {
-            store.commit("setReset");
-            localStorage.setItem("access-token", res.headers["access-token"]);
-            store.commit("setUserInfo", res.data.UserDto);
-                this.state.isLogin = true;
-            })
         .catch((err) => {
-            console.log(err);
-<<<<<<< HEAD
+          console.log(err);
         });
     },
-=======
-            });
+    getUserRank(store, payload) {
+      let token = localStorage.getItem("access-token");
+      axios({
+        method: "get",
+        url: API.url + userAPI.rank(payload),
+        headers: { "access-token": token },
+      })
+        .then((res) => {
+          if (res) {
+            // console.log(res.data.rank);
+            store.commit("setUserRank", res.data.rank);
+            this.state.rank = res.rank;
+          } else console.log("랭크 가져오기 실패.");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getUpdateUserInfo(store, payload) {
+      let token = localStorage.getItem("access-token");
+      console.log("token : ", token);
+      axios({
+        method: "get",
+        url: API.url + userAPI.tokenUpdate(payload),
+        headers: { "access-token": token },
+      })
+        .then((res) => {
+          store.commit("setReset");
+          localStorage.setItem("access-token", res.headers["access-token"]);
+          store.commit("setUserInfo", res.data.UserDto);
+          this.state.isLogin = true;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     // getOtherUserInfo(payload) {
     //     axios({
@@ -174,11 +171,9 @@ actions: {
     //             console.log(" 다른 유저 : ", res.data);
     //         })
     //         .catch((err) => {
-    //         console.log("ohter User 정보 조회 실패"); 
+    //         console.log("ohter User 정보 조회 실패");
     //         console.log(err);
     //         });
     //     },
->>>>>>> branch 'master' of https://lab.ssafy.com/s05-webmobile2-sub3/S05P13A606.git
-        
-},
+  },
 };
