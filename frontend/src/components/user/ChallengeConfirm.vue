47047dea922aea9ebf6dcb26fde1ce06ec165381
GyuYoungCho <rbud613@gmail.com>
2021-08-13 16:05:13 +0900

    S05P13A606-148 fix : newsfeed search

--- frontend/src/components/user/ChallengeConfirm.vue
+++ frontend/src/components/user/ChallengeConfirm.vue
@@ -10,14 +10,14 @@
           max-width="1000px">
         <v-card tile>
           <v-toolbar color="#004627" dark>
-          <v-toolbar-title >{{this.selectpot.title}}</v-toolbar-title>
+          <!-- <v-toolbar-title >{{this.selectpot.title}}</v-toolbar-title>
             <profile-name :user="selectpot.userid" class="ml-3 mt-1"></profile-name>
             <v-spacer></v-spacer>
             <v-btn icon dark @click="cancelDetail()" justify="end">
               <v-icon>mdi-close</v-icon>
-            </v-btn>
+            </v-btn> -->
           </v-toolbar>
-            <v-col>
+            <!-- <v-col>
               <v-row class="pl-3">
                 <v-col class="mt-3">
                   <v-row>
@@ -63,7 +63,7 @@
               <v-col class="mt-5 pl-3">
                 <profile-name v-for="(auser, index) in potattendusers" :key="index" :user="auser"></profile-name>
               </v-col>  
-            </v-col>
+            </v-col> -->
           </v-card>
     </v-dialog>
     
@@ -71,9 +71,9 @@
 </template>
 
 <script>
-import moment from "moment"
+// import moment from "moment"
 import { mapGetters} from 'vuex';
-import ProfileName from '@/components/basic/ProfileName.vue';
+// import ProfileName from '@/components/basic/ProfileName.vue';
 
 export default {
   data(){
@@ -81,60 +81,60 @@ export default {
       modals : true,
     }
   },
-  components: { 
-    ProfileName,
-  },
+//   components: { 
+//     ProfileName,
+//   },
   props:{
     mo : Boolean,
   },
   computed:{
     ...mapGetters("user", ["userinfo"]),
-    ...mapGetters("pot", ["selectpot","potattendusers"]),
-    meet_date(){
-        return moment(this.selectpot.time).format("ddd MM/DD")
-    },
-    meet_time(){
-        return moment(this.selectpot.time).format("HH:mm")
-    },
-    rows() {
-        return this.potattendusers.length
-    },
-    detailable(){
-      return this.modals && this.modalDetail
-    },
-    attendme(){
-        let flag = true;
-        if(this.potattendusers.length!=0){
-          this.potattendusers.forEach((item) => {
-            if (item.userid == this.userinfo.userid) {
-              flag = false
-            }
-          });
-        }
-        return flag
-    },
-    mypot(){
+    // ...mapGetters("pot", ["selectpot","potattendusers"]),
+    // meet_date(){
+    //     return moment(this.selectpot.time).format("ddd MM/DD")
+    // },
+    // meet_time(){
+    //     return moment(this.selectpot.time).format("HH:mm")
+    // },
+    // rows() {
+    //     return this.potattendusers.length
+    // },
+    // detailable(){
+    //   return this.modals && this.modalDetail
+    // },
+    // attendme(){
+    //     let flag = true;
+    //     if(this.potattendusers.length!=0){
+    //       this.potattendusers.forEach((item) => {
+    //         if (item.userid == this.userinfo.userid) {
+    //           flag = false
+    //         }
+    //       });
+    //     }
+    //     return flag
+    // },
+    // mypot(){
      
-      if(this.selectpot.length!=0 && this.userinfo.userid == this.selectpot.userid.userid) {
-        return true
-      }else{
-        return false
-      }
-    }
+    //   if(this.selectpot.length!=0 && this.userinfo.userid == this.selectpot.userid.userid) {
+    //     return true
+    //   }else{
+    //     return false
+    //   }
+    // }
   },
   methods:{
 
-    cancelDetail() {
-      this.$emit('openDetailModal', false)
-    },
+    // cancelDetail() {
+    //   this.$emit('openDetailModal', false)
+    // },
 
-    openMessageModal(sign){
-        this.$emit('openMessageModal', true, sign)
-    },
-    potModify(){
-        // this.selectPot(this.potitem)
-        this.$router.push({path:"/vetparty_create/1"});
-    },
+    // openMessageModal(sign){
+    //     this.$emit('openMessageModal', true, sign)
+    // },
+    // potModify(){
+    //     // this.selectPot(this.potitem)
+    //     this.$router.push({path:"/vetparty_create/1"});
+    // },
 
   },
 
