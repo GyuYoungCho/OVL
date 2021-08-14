3e6324ae034f3bc8718a61e6032e2d4550a0f3a8
kevin622 <kevin622@yonsei.ac.kr>
2021-08-14 22:03:02 +0900

    S05P13A606-159 refactor: login signup

--- frontend/src/store/modules/user.js
+++ frontend/src/store/modules/user.js
@@ -2,6 +2,8 @@
 import axios from "axios";
 import API from "@/api/index.js";
 import userAPI from "@/api/user.js";
+import router from "@/router"
+
 
 export default {
   namespaced: true,
@@ -26,73 +28,6 @@ export default {
     modalContent: "",
     userlist: [],
   },
-  mutations: {
-    setUserInfo(state, payload) {
-      state.isLogin = true;
-      state.userinfo = payload;
-      console.log("유저 : ", state.userinfo.nickname);
-    },
-    setLogout(state) {
-      state.isLogin = false;
-      state.userinfo = {
-        userid: "",
-        email: "",
-        password: "",
-        name: "",
-        nickname: "",
-        phone: "",
-        filepath: "",
-      };
-      state.rank = '',
-        state.percent = '',
-      localStorage.removeItem("access-token");
-      localStorage.removeItem("vuex");
-    },
-    setReset(state) {
-      state.userinfo = {
-        userid: "",
-        email: "",
-        password: "",
-        name: "",
-        nickname: "",
-        phone: "",
-        filepath: "",
-        challengeId: {
-          start_date: null,
-        },
-        rank: null,
-        percent: null,
-      };
-    },
-
-    setUserRank(state, payload) {
-      state.rank = payload;
-    },
-
-    setUserPercent(state, payload) {
-      state.percent = payload;
-    },
-
-    setModal(state, { modalOpen, modalContent }) {
-      state.modalOpen = modalOpen;
-      state.modalContent = modalContent;
-    },
-    setUserList(state, payload) {
-      if (payload) {
-        state.userlist = payload.sort(function(user1, user2) {
-          let x = user1.experience;
-          let y = user2.experience;
-          if (x > y) {
-            return -1;
-          }
-          if (x < y) {
-            return 1;
-          }
-          return 0;
-        });
-      } else state.userlist = payload;
-    },
-  },
   getters: {
     userinfo(state) {
       return state.userinfo;
@@ -110,68 +45,138 @@ export default {
       return state.modalContent;
     },
   },
+  mutations: {
+    SET_USER_INFO(state, payload) {
+      if (!state.isLogin) {
+        // 로그인이 되어있지 않다면 지금 로그인했다는 뜻! 로그인 끝내고 메인페이지로 보내기
+        state.isLogin = true;
+        router.push({name: 'Main'})
+      }
+      state.userinfo = payload;
+      console.log("유저 : ", state.userinfo.nickname);
+    },
+    SET_LOGOUT(state) {
+      state.isLogin = false;
+      state.userinfo = {
+        userid: "",
+        email: "",
+        password: "",
+        name: "",
+        nickname: "",
+        phone: "",
+        filepath: "",
+      };
+      state.rank = '',
+        state.percent = '',
+      localStorage.removeItem("access-token");
+      localStorage.removeItem("vuex");
+    },
+    SET_RESET(state) {
+      state.userinfo = {
+        userid: "",
+        email: "",
+        password: "",
+        name: "",
+        nickname: "",
+        phone: "",
+        filepath: "",
+        challengeId: {
+          start_date: null,
+        },
+        rank: null,
+        percent: null,
+      };
+    },
+
+    SET_USER_RANK(state, payload) {
+      state.rank = payload;
+    },
+
+    SET_USER_PERCENT(state, payload) {
+      state.percent = payload;
+    },
+
+    SET_MODAL(state, { modalOpen, modalContent }) {
+      state.modalOpen = modalOpen;
+      state.modalContent = modalContent;
+    },
+    SET_USER_LIST(state, payload) {
+      if (payload) {
+        state.userlist = payload.sort(function(user1, user2) {
+          const x = user1.experience;
+          const y = user2.experience;
+          if (x > y) {
+            return -1;
+          }
+          if (x < y) {
+            return 1;
+          }
+          return 0;
+        });
+      } else state.userlist = payload;
+    },
+  },
   actions: {
-    getUserInfo(store) {
-      let token = localStorage.getItem("access-token");
-      //console.log("jwt 정보" + token);
+    // 토큰으로 유저 확인
+    async getUserInfo({ commit }) {
+      const token = localStorage.getItem("access-token");
+      // 토큰 없으면 함수 실행 끝냄
       if (!token) {
         return;
       }
-      axios({
+      // 토큰 있으면 POST 요청으로 유저 정보 가져오기
+      await axios({
         method: "post",
         url: API.url + userAPI.info(),
         headers: { "access-token": token },
       })
         .then((res) => {
           if (res.data) {
-            store.commit("setUserInfo", res.data.UserDto);
-            this.state.isLogin = true;
+            commit("SET_USER_INFO", res.data.UserDto);
           } else console.log("실패.");
         })
         .catch((err) => {
           console.log(err);
         });
     },
-    login(store, loginObj) {
-      return new Promise(function(resolve) {
-        axios({
-          method: "post",
-          url: API.url + userAPI.login(),
-          data: loginObj,
+    async login({ commit, dispatch }, loginObj) {
+      await axios({
+        method: "post",
+        url: API.url + userAPI.login(),
+        data: loginObj,
+      })
+        .then((res) => {
+          localStorage.setItem("access-token", res.headers["access-token"]);
+          dispatch('getUserInfo');
         })
-          .then((res) => {
-            localStorage.setItem("access-token", res.headers["access-token"]);
-            resolve();
-            store.commit("setUserInfo", res.data.data);
-          })
-          .catch((err) => {
-            store.commit("setModal", { modalOpen: true, modalContent: "이메일과 비밀번호를 확인하세요." });
-            setTimeout(() => {
-              store.commit("setModal", { modalOpen: false, modalContent: "" });
-            }, 1000);
-            console.log(err);
-          });
-      });
+        .catch((err) => {
+          // 로그인 실패 시 모달 띄우기
+          commit("SET_MODAL", { modalOpen: true, modalContent: "이메일과 비밀번호를 확인하세요." });
+          setTimeout(() => {
+            commit("SET_MODAL", { modalOpen: false, modalContent: "" });
+          }, 1000);
+          console.error(err);
+        });
     },
-    logout(store) {
-      store.commit("setLogout");
+    logout({ commit }) {
+      commit("SET_LOGOUT");
     },
-    deleteUser(store) {
+    deleteUser({ commit, state }) {
       let token = localStorage.getItem("access-token");
       axios({
         method: "delete",
-        url: API.url + userAPI.delete(store.state.userinfo.userid),
+        url: API.url + userAPI.delete(state.userinfo.userid),
         headers: { "access-token": token },
       })
         .then(() => {
           alert("탈퇴가 정상적으로 처리 되었습니다.");
-          store.commit("setLogout");
+          commit("SET_LOGOUT");
         })
         .catch((err) => {
           console.log(err);
         });
     },
-    getUserRank(store, payload) {
+    getUserRank({ commit }, payload) {
       let token = localStorage.getItem("access-token");
       axios({
         method: "get",
@@ -182,17 +187,15 @@ export default {
           if (res) {
             console.log(res.data.rank);
             console.log("percent", res.data.percent);
-            store.commit("setUserRank", res.data.rank);
-            store.commit("setUserPercent", res.data.percent);
-            this.state.rank = res.rank;
-            this.state.percent = res.percent;
+            commit("SET_USER_RANK", res.data.rank);
+            commit("SET_USER_PERCENT", res.data.percent);
           } else console.log("랭크 가져오기 실패.");
         })
         .catch((err) => {
           console.log(err);
         });
     },
-    getUpdateUserInfo(store, payload) {
+    getUpdateUserInfo({ commit }, payload) {
       let token = localStorage.getItem("access-token");
       axios({
         method: "get",
@@ -200,16 +203,16 @@ export default {
         headers: { "access-token": token },
       })
         .then((res) => {
-          store.commit("setReset");
+          commit("SET_RESET");
           localStorage.setItem("access-token", res.headers["access-token"]);
-          store.commit("setUserInfo", res.data.UserDto);
+          commit("SET_USER_INFO", res.data.UserDto);
           this.state.isLogin = true;
         })
         .catch((err) => {
           console.log(err);
         });
     },
-    getUserList(store) {
+    getUserList({ commit }) {
       let token = localStorage.getItem("access-token");
       axios({
         method: "get",
@@ -217,7 +220,7 @@ export default {
         headers: { "access-token": token },
       })
         .then((res) => {
-          store.commit("setUserList", res.data);
+          commit("SET_USER_LIST", res.data);
         })
         .catch((err) => {
           console.log(err);
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,29 +1,7 @@
 <template>
   <v-container>
-    <!-- 회원가입 관련 모달 -->
-    <v-dialog v-model="modalOpen" max-width="300" persistent @click:outside="nothing">
-      <v-card>
-        <!-- 모달 타이틀 영역 -->
-        <v-toolbar dense color="#004627">
-          <v-toolbar-title class="modalTitle">
-            <!-- {{ modaltitle }} -->
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          
-        </v-toolbar>
-        <!-- 모달 컨텐츠 영역 -->
-        <v-container>
-        <div class="modalContent">
-          <div class="mb-3">
-            <span class="modalContentMessage">
-              {{ modalContent }}
-            </span>
-          </div>
-        </div>
-        </v-container>
-      </v-card>
-    </v-dialog>
-
+    <!-- 모달 -->
+    <FlashModal :modalOpen="modalOpen" :modalContent="modalContent" />
 
     <section class="signup">
       <img src="@/assets/image/OVL_logo.png" alt="">
@@ -89,9 +67,13 @@
 import axios from 'axios'
 import API from '@/api/index.js'
 import userAPI from '@/api/user.js'
+import FlashModal from '@/components/signup/FlashModal.vue'
 import 'url-search-params-polyfill';
 
 export default {
+  components: {
+    FlashModal,
+  },
   data: () => ({
     nicknameValid: false,
     emailValid: false,
