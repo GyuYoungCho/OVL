b3cd76340500b13d10af944f8cead74c5a94c1ad
GyuYoungCho <rbud613@gmail.com>
2021-08-17 02:17:13 +0900

    S05P13A606-180 add : recipe scroll

--- backend/src/main/java/com/project/ovl/controller/feed/FeedController.java
+++ backend/src/main/java/com/project/ovl/controller/feed/FeedController.java
@@ -1,6 +1,10 @@
 package com.project.ovl.controller.feed;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import javax.transaction.Transactional;
 
@@ -12,16 +16,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.ovl.dao.FollowDao;
 import com.project.ovl.dao.post.PostDao;
 import com.project.ovl.dao.post.PostPhotoDao;
+import com.project.ovl.model.follow.Follow;
 import com.project.ovl.model.photo.PostPhoto;
 import com.project.ovl.model.post.Post;
 import com.project.ovl.model.recipe.Recipe;
+import com.project.ovl.model.user.User;
 
 import io.swagger.annotations.ApiOperation;
 
@@ -33,10 +42,13 @@ public class FeedController {
 	@Autowired
 	PostPhotoDao postPhotoDao;
 	
+	@Autowired
+	FollowDao followDao;
+	
 	@GetMapping("/postfeed")
 	@ApiOperation(value = "뉴스 피드 페이징")
 	public Page<PostPhoto> feedmain(@RequestParam("userId") int userId, final Pageable pageable) {
-		// 페이지 index는 0부터
+		
 		Page<PostPhoto> postpage = postPhotoDao.findByUserFollowing(userId, pageable);
 		List<PostPhoto> posts = postpage.toList();
 		return new PageImpl<PostPhoto>(posts,pageable,postpage.getTotalElements());
@@ -45,7 +57,7 @@ public class FeedController {
 	@GetMapping("/popularfeed")
 	@ApiOperation(value = "초반이라 팔로우 등 없을 때")
 	public Page<PostPhoto> popularfeed(final Pageable pageable) {
-		// 페이지 index는 0부터
+		
 		Page<PostPhoto> postpage = postPhotoDao.findByComing(pageable);
 		List<PostPhoto> posts = postpage.toList();
 		return new PageImpl<PostPhoto>(posts,pageable,postpage.getTotalElements());
@@ -68,5 +80,21 @@ public class FeedController {
 //		return new ResponseEntity<List<Recipe>>(recipeList, HttpStatus.OK);
 //	}
 //	
-	
+//	@GetMapping("/following/page/{from_id}")
+//	@ApiOperation(value = "내가 팔로우하는 스크롤")
+//	public Page<User> findfollowing(@RequestParam int from_id, final Pageable pageable) {
+//		Page<User> flist = followDao.findfollwing(from_id, pageable);
+//		List<User> ulist = flist.toList();
+//		return new PageImpl<User>(ulist,pageable,flist.getTotalElements());
+//	}
+//	
+//	@GetMapping("/follower/page/{to_id}")
+//	@ApiOperation(value = "나를 팔로우하는 사람들 스크롤")
+//	public Page<User> findfollower(@PathVariable int to_id, final Pageable pageable) {
+//		
+//		Page<User> flist = followDao.findfollwer(to_id, pageable);
+//		List<User> ulist = flist.toList();
+//		return new PageImpl<User>(ulist,pageable,flist.getTotalElements());
+//		
+//	}
 }
