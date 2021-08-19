38dc29233923dab41713782f9d2d511864a1a6ca
alexkwonpro <alexkwonpro@gmail.com>
2021-08-19 11:18:49 +0900

    S05P13A606-201 fix: pot search css fix

--- frontend/src/components/basic/PotSearch.vue
+++ frontend/src/components/basic/PotSearch.vue
@@ -3,18 +3,18 @@
     <v-card class="search_bar">
       <v-row class="ma-0">
         <v-col class="pa-0 ma-0" cols="3">
-          <v-select class="pa-0 ma-0 ml-1" v-model="ord"
+          <v-select class="pa-0 ma-0 ml-2" v-model="ord"
             flat solo rounded justify="start" :items="order"
             background-color="rgba(255,255,255,0)" item-color="#004627"
             color="white" append-icon="mdi-menu-down-outline"></v-select>
         </v-col>
-        <v-col class="pa-0 pr-0 ma-0 pl-3">
-          <v-text-field class="pt-0"
+        <v-col class="pa-0 pr-0 ma-0">
+          <v-text-field class="pt-0 vet-padding-add"
             v-model="search" clearable height="20px" rounded
             hide-details placeholder="검색어를 입력하세요."
           ></v-text-field>
         </v-col>
-      <v-btn class="pl-7" icon><v-icon color="white" small dark>mdi-magnify</v-icon></v-btn>
+      <v-btn icon><v-icon color="white" small dark>mdi-magnify</v-icon></v-btn>
       </v-row>
     </v-card>
   </div>
