2e0b43fbde0c69f886013b51ffedbe15e234dbc6
GyuYoungCho <rbud613@gmail.com>
2021-08-20 01:47:38 +0900

    fix : image file

--- frontend/src/views/ArticleCreate.vue
+++ frontend/src/views/ArticleCreate.vue
@@ -21,7 +21,8 @@
         <div v-if="type==0">
           <div class='articlePic' v-if="!sendList.length">
             <label for="file"><v-icon>mdi-plus</v-icon></label>
-            <input id="file" type="file" ref="files" multiple @input="fileUpload(0)">
+            <input id="file" type="file" ref="files" multiple @input="fileUpload(0)" 
+            accept="image/jpeg, image/png">
           </div>
           <!-- 캐러셀 영역 -->
           <v-carousel class="carouselBorder" hide-delimiters v-else height="30vh">
@@ -29,14 +30,16 @@
               <!-- 사진 수정 -->
               <span @click="photoModify(idx)">
                 <label for="modifyFile"><v-icon class="photoClick">mdi-check-circle-outline</v-icon></label>
-                <input id="modifyFile" type="file" ref="modifyFiles" @input="modifyFileUploadRegist" style="width:0; height:0">  
+                <input id="modifyFile" type="file" ref="modifyFiles" @input="modifyFileUploadRegist" style="width:0; height:0"
+               accept="image/jpeg, image/png">  
               </span>
               <!-- 사진 삭제 -->
               <span @click="photoDelete(idx)"><v-icon class="photoClick">mdi-close-circle-outline</v-icon></span>  
               <!-- 사진 추가 -->
               <span>
                 <label for="newFile"><v-icon class="photoClick">mdi-plus-circle-outline</v-icon></label>
-                <input id="newFile" type="file" ref="plusFiles" multiple @input="newFileUpload(0)" style="width:0; height:0">
+                <input id="newFile" type="file" ref="plusFiles" multiple @input="newFileUpload(0)" style="width:0; height:0"
+                accept="image/jpeg, image/png">
               </span>
             </v-carousel-item>
           </v-carousel>
@@ -45,34 +48,45 @@
         <div v-else>
           <div class='articlePic' v-if="!photoList.length">
             <label for="file"><v-icon>mdi-plus</v-icon></label>
-            <input id="file" type="file" ref="files" multiple @input="fileUpload(1)" style="width:0; height:0">
+            <input id="file" type="file" ref="files" multiple @input="fileUpload(1)" style="width:0; height:0"
+              accept="image/jpeg, image/png">
           </div>
           <v-carousel v-else class="carouselBorder" hide-delimiters height="30vh">
             <v-carousel-item v-for="(info,idx) in photoList" :key="idx" :src="photoPath(idx)" style="text-align:right">
               <!-- 사진 수정 -->
               <span @click="photoModify(idx)">
                 <label for="modifyFile"><v-icon class="photoClick">mdi-check-circle-outline</v-icon></label>
-                <input id="modifyFile" type="file" ref="modifyFiles" @input="modifyFileUpload" style="width:0; height:0">  
+                <input id="modifyFile" type="file" ref="modifyFiles" @input="modifyFileUpload" style="width:0; height:0"
+                  accept="image/jpeg, image/png">  
               </span>
               <!-- 사진 삭제 -->
               <span @click="photoDelete(idx)"><v-icon class="photoClick">mdi-close-circle-outline</v-icon></span>  
               <!-- 사진 추가 -->
               <span>
                 <label for="newFile"><v-icon class="photoClick">mdi-plus-circle-outline</v-icon></label>
-                <input id="newFile" type="file" ref="plusFiles" multiple @input="newFileUpload(1)" style="width:0; height:0">
+                <input id="newFile" type="file" ref="plusFiles" multiple @input="newFileUpload(1)" 
+                accept="image/jpeg, image/png" style="width:0; height:0">
               </span>
             </v-carousel-item>
+            
           </v-carousel>
+          
         </div>
+        <span style="text-align:right;">png, jpg, jpeg만 가능</span>
         
         <!-- 게시글 컨텐츠 입력 영역 -->
         <textarea v-model="content" placeholder="게시글 입력..."></textarea>
         
-        <div v-if="type==0" @click="send" :class="{'buttonDiv': articleCreateFormValid, 'disabledBtnDiv': !articleCreateFormValid}">
-          <button :disabled="!articleCreateFormValid">등록</button>
+        <div v-if="type==0">
+          <div  :class="{'buttonDiv': articleCreateFormValid, 'disabledBtnDiv': !articleCreateFormValid}">
+              <button style="width:100%" :disabled='!articleCreateFormValid'  @click="send">등록</button>
+          </div>
         </div>
-        <div v-else @click="modify" :class="{'buttonDiv': articleModifyFormValid, 'disabledBtnDiv': !articleModifyFormValid}">
-          <button :disabled="!articleModifyFormValid">수정</button>
+        <div v-else >
+          <div  
+            :class="{'buttonDiv': articleModifyFormValid, 'disabledBtnDiv': !articleModifyFormValid}">
+         <button style="width:100%" :disabled='!articleModifyFormValid' @click="modify">수정</button>
+          </div>
         </div>
       </section>
     </v-container>
@@ -168,18 +182,23 @@
         // console.log(this.category)
       },
       fileUpload (type) {
+        let cnt=0
         for (let i = 0; i < this.$refs.files.files.length; i++)  {
-          const previewUrl = URL.createObjectURL(this.$refs.files.files[i])
-          if (type==0) {
-            this.sendList.push(this.$refs.files.files[i])
-            this.previewItems.push({src:previewUrl})
-          } else {
-            this.plusPhotoList.push(this.$refs.files.files[i])
-            this.photoList.push({
-              "filesize":0,
-              "url":previewUrl
-            })
+          if(this.$refs.files.files[i].type =='image/jpeg'||this.$refs.files.files[i].type == 'image/png'){
+            if(cnt==10) break
+            const previewUrl = URL.createObjectURL(this.$refs.files.files[i])
+            if (type==0) {
+              this.sendList.push(this.$refs.files.files[i])
+              this.previewItems.push({src:previewUrl})
+            } else {
+              this.plusPhotoList.push(this.$refs.files.files[i])
+              this.photoList.push({
+                "filesize":0,
+                "url":previewUrl
+              })
+            }
           }
+          cnt= cnt+1
         }
       },
       photoPath(idx) {
@@ -250,44 +269,52 @@
         if (idx!=0) this.tempIdx = idx;
       },
       modifyFileUploadRegist() { // 게시글 등록 시 사진 수정
-        const previewUrl = URL.createObjectURL(this.$refs.modifyFiles[0].files[0]);
-        this.previewItems[this.tempIdx] = {"src":previewUrl};
-        this.sendList[this.tempIdx] = this.$refs.modifyFiles[0].files[0];
-        this.previewItems.push("지워");
-        this.previewItems.splice(this.previewItems.length-1, 1);
+        if(this.$refs.modifyFiles[0].files[0].type =='image/jpeg' || this.$refs.modifyFiles[0].files[0].type =='image/png'){
+          const previewUrl = URL.createObjectURL(this.$refs.modifyFiles[0].files[0]);
+          this.previewItems[this.tempIdx] = {"src":previewUrl};
+          this.sendList[this.tempIdx] = this.$refs.modifyFiles[0].files[0];
+          this.previewItems.push("지워");
+          this.previewItems.splice(this.previewItems.length-1, 1);
+        }
       },
       modifyFileUpload() { // 게시글 수정 시 사진 수정
-        if (this.photoList[this.tempIdx].filesize == 0) { // 수정한 사진. 또 수정하는거니까 modifyPHotoList, idList에 추가하면 안됨
-          var idx = this.modifyIdList.indexOf(this.photoList[this.tempIdx].postPhotoId);
-          this.modifyPhotoList[idx] = this.$refs.modifyFiles[0].files[0];
-        } else {
-          this.modifyPhotoList.push(this.$refs.modifyFiles[0].files[0]);
-          this.modifyIdList.push(this.photoList[this.tempIdx].postPhotoId);
-        }
-        const previewUrl = URL.createObjectURL(this.$refs.modifyFiles[0].files[0]);
-        
-        this.photoList[this.tempIdx] = {
-          "postPhotoId":this.photoList[this.tempIdx].postPhotoId,
-          "filesize":0, // filesize로 수정 사진, 기존 사진 구분 
-          "url":previewUrl
-        }
-        // 배열에 있는 항목을 집적 설정할 경우 변경 사항을 감지할 수 없으므로 강제 업데이트 하기 위함
-        this.photoList.push("지워");
-        this.photoList.splice(this.photoList.length-1, 1);
+        if(this.$refs.modifyFiles[0].files[0].type =='image/jpeg' || this.$refs.modifyFiles[0].files[0].type =='image/png'){
+          if (this.photoList[this.tempIdx].filesize == 0) { // 수정한 사진. 또 수정하는거니까 modifyPHotoList, idList에 추가하면 안됨
+            var idx = this.modifyIdList.indexOf(this.photoList[this.tempIdx].postPhotoId);
+            this.modifyPhotoList[idx] = this.$refs.modifyFiles[0].files[0];
+          } else {
+            this.modifyPhotoList.push(this.$refs.modifyFiles[0].files[0]);
+            this.modifyIdList.push(this.photoList[this.tempIdx].postPhotoId);
+          }
+          const previewUrl = URL.createObjectURL(this.$refs.modifyFiles[0].files[0]);
+          
+          this.photoList[this.tempIdx] = {
+            "postPhotoId":this.photoList[this.tempIdx].postPhotoId,
+            "filesize":0, // filesize로 수정 사진, 기존 사진 구분 
+            "url":previewUrl
+          }
+          // 배열에 있는 항목을 집적 설정할 경우 변경 사항을 감지할 수 없으므로 강제 업데이트 하기 위함
+          this.photoList.push("지워");
+          this.photoList.splice(this.photoList.length-1, 1);
+          }
       },
       newFileUpload(type) {
+        // let cnt 
         for (let i = 0; i < this.$refs.plusFiles[0].files.length; i++) {
-          const previewUrl = URL.createObjectURL(this.$refs.plusFiles[0].files[i])
-          if (type==0) {
-            this.sendList.push(this.$refs.plusFiles[0].files[i])
-            this.previewItems.push({src:previewUrl})
-          } else {
-            this.plusPhotoList.push(this.$refs.plusFiles[0].files[i])
+          if(this.$refs.plusFiles[0].files[i].type =='image/jpeg'||this.$refs.plusFiles[0].files[i].type == 'image/png'){
+
             const previewUrl = URL.createObjectURL(this.$refs.plusFiles[0].files[i])
-            this.photoList.push({
-              "filesize":0,
-              "url":previewUrl
-            })
+            if (type==0) {
+              this.sendList.push(this.$refs.plusFiles[0].files[i])
+              this.previewItems.push({src:previewUrl})
+            } else {
+              this.plusPhotoList.push(this.$refs.plusFiles[0].files[i])
+              const previewUrl = URL.createObjectURL(this.$refs.plusFiles[0].files[i])
+              this.photoList.push({
+                "filesize":0,
+                "url":previewUrl
+              })
+            }
           }
         }
       },
